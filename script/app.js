d66b696201f024e96051e6c8d3714cfb465077cb
Htet Bo Bo Aung <boboa3473@gmail.com>
2024-01-11 17:41:10 +0630

    Update the app.js

--- script/app.js
+++ script/app.js
@@ -2,8 +2,10 @@ bookList = document.querySelector(".book-list");
 addBtn = document.getElementById("add-btn");
 inputName = document.getElementById("name-input");
 inputAuthor = document.getElementById("author-input");
-books = JSON.parse(localStorage.getItem("books")) || [];
-displayBooks();
+
+const storedBooks = JSON.parse(localStorage.getItem("books"));
+const books = storedBooks || [];
+
 class Book {
      constructor() {
        
@@ -22,16 +24,16 @@ class Book {
           bookList.append(li);
           });
    
-     const deleteBtn = document.querySelector(".delete-btn");
+     const deleteBtn = document.querySelectorAll(".delete-btn");
      deleteBtn.forEach((button) => {
-         button.addEventListener("click", this.deleteHandler);
+         button.addEventListener("click", this.deleteHandler.bind(this));
      });
      }
    
      deleteHandler(e) {
        const index = e.target.dataset.index;
        books.splice(index, 1);
-       localStorage.setItem("books", JSON.stringify(this.books));
+       localStorage.setItem("books", JSON.stringify(books));
        this.displayBooks();
      }
    
@@ -49,11 +51,13 @@ class Book {
        let book = { name, author };
        books.push(book);
    
-       localStorage.setItem("books", JSON.stringify(this.books));
+       localStorage.setItem("books", JSON.stringify(books));
        this.displayBooks();
        inputName.value = "";
        inputAuthor.value = "";
      }
    }
 const bookManager = new Book();
-addBtn.addEventListener("click", showHandler);
+addBtn.addEventListener("click", (e) => {
+  bookManager.showHandler(e);
+});
